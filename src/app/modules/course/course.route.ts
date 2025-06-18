5af76cec6ccb2ec73f1ef4660086e963206087dd
Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-06-18 20:02:01 +0600

    Get All Course Functionality Added

--- src/app/modules/course/course.route.ts
+++ src/app/modules/course/course.route.ts
@@ -1,15 +1,15 @@
-import { EnumUserRole} from './../Auth/auth.interface';
+import { EnumUserRole } from './../Auth/auth.interface';
 import express from 'express';
 import { CourseController } from './course.controller';
 import RoleValidation from '../../middlewares/RoleValidation';
 
 const router = express.Router();
 
+router.get('/', CourseController.getAllCourses);
 router.post(
-  '/create-course', RoleValidation(EnumUserRole.TEACHER),
+  '/create-course',
+  RoleValidation(EnumUserRole.TEACHER),
   CourseController.createCourse,
 );
-
-
 
 export const CourseRoutes = router;
